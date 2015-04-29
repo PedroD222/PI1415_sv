9d93a40d97cf58caf843e697dfd3a4318a89c41d
JohnGod <joaodavid72@hotmail.com>
2015-04-29 21:06:28 +0100

    Added 3 more classes

---
+++ WWonlineapi/WWonlineapi/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWonlineapi
+{
+    public class Data
+    {
+        public List<Request> request { get; set; }
+        public List<Weather> weather { get; set; }
+    }
+}
