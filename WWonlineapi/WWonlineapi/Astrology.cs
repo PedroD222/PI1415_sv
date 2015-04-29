68291d9a039c40e55982bddf9e909ba2145a4c84
JohnGod <joaodavid72@hotmail.com>
2015-04-29 20:50:06 +0100

    Added classes

---
+++ WWonlineapi/WWonlineapi/Astrology.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWonlineapi
+{
+    public class Astronomy
+    {
+        public string moonrise { get; set; }
+        public string moonset { get; set; }
+        public string sunrise { get; set; }
+        public string sunset { get; set; }
+    }
+}
---
+++ WWonlineapi/WWonlineapi/Weather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWonlineapi
+{
+    public class Weather
+    {
+        public List<Astronomy> astronomy { get; set; }
+        public string date { get; set; }
+        public List<Hourly> hourly { get; set; }
+        public string maxtempC { get; set; }
+        public string maxtempF { get; set; }
+        public string mintempC { get; set; }
+        public string mintempF { get; set; }
+        public string uvIndex { get; set; }
+    }
+}
