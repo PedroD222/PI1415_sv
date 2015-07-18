164b231ea7727443edd81504bb54960bb0cd4b3d
PedroD222 <pedroduarte222@hotmail.com>
2015-07-18 13:56:22 +0100

    clx run

--- PF/clx - chelas xl/routes/index.js
+++ PF/clx - chelas xl/routes/index.js
@@ -9,7 +9,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     //eventually will do something useful
-    //return next();
+    return res.render("CLX");
 });
 
 module.exports = router;
