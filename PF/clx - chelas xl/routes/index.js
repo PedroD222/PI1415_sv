8150643080d8e484c4f271bab0f22a3554fb7a57
PedroD222 <pedroduarte222@hotmail.com>
2015-07-18 18:20:00 +0100

    show index with err

--- PF/clx - chelas xl/routes/index.js
+++ PF/clx - chelas xl/routes/index.js
@@ -7,9 +7,55 @@ var router = express.Router();
 //var db = require('../dbaccess');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function home(req, res, next) {
     //eventually will do something useful
-    return res.render("CLX");
+    //return res.render("index", {req:user});
+    db.getUser(req.user.username, function(err, user){
+        if(err) {
+            if(err.message !== 'RECORD NOT FOUND')
+                return next(err);
+        }
+        console.log('got user');
+        db.getCountQueixinhas(function(err, count) {
+            count = Math.ceil(count/10);
+            console.log(count);
+            if(count > 0 && req.user.username){
+                db.getvotobyuser(user.username, function(err, votos){
+                    if(err) {
+                        if(err.message !== 'RECORD NOT FOUND')
+                            return next(err);
+                    }
+                    db.getQueixinhasbyIntUser(user.username, function(err, interested){
+                        if(err) {
+                            if(err.message !== 'RECORD NOT FOUND')
+                                return next(err);
+                        }
+                        queixas.forEach(function(queixa){
+                            queixa.voto = 0;
+                            queixa.isfollowing = false;
+                            if(votos) {
+                                votos.forEach(function(voto){
+                                    if(queixa.id === voto.queixinha)
+                                        queixa.voto = voto.voto;
+                                });
+                            }
+                            if (interested){
+                                interested.forEach(function(following){
+                                    if(following.id === queixa.id)
+                                        queixa.isfollowing = true;
+                                });
+                            }
+                        });
+                    });
+                    return res.render('queixinhas', {queixas: queixas, user:req.user, page:page, total:count});
+                });
+            }
+            else{
+                return res.render('queixinhas', {queixas: queixas, user:req.user, page:page, total:count});
+            }
+        });
+
+    res.render('index', { title: 'CLX - Chelas' });
 });
 
 module.exports = router;
