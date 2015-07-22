6ac946d4e650bda5db2761d1639d6375bb0c5104
JohnGod <joaodavid72@hotmail.com>
2015-07-22 21:09:27 +0100

    Add Table PontuacaoUtilizador

---
+++ PF/bd/create/CreatePontuacaoUtilizador.sql
@@ -0,0 +1,10 @@
+﻿CREATE SEQUENCE PontuacaoUtilizador_ID_seq;
+
+CREATE TABLE PontuacaoUtilizador(
+	id integer NOT NULL DEFAULT nextval('PontuacaoUtilizador_ID_seq'::regclass),
+	username char(50) not null,
+	pontacao int not null,
+	CONSTRAINT "fkUsername_PontuacaoUtilizador" 
+	FOREIGN KEY (username) REFERENCES Utilizador (username) 
+	MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, 
+)
