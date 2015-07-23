e47b67f502238ddfa7e1219f6ded07602a4e47d3
JohnGod <joaodavid72@hotmail.com>
2015-07-23 11:40:45 +0100

    BD Update

--- PF/bd/create/CreatePontuacaoUtilizador.sql
+++ PF/bd/create/CreatePontuacaoUtilizador.sql
@@ -4,7 +4,8 @@ CREATE TABLE PontuacaoUtilizador(
 	id integer NOT NULL DEFAULT nextval('PontuacaoUtilizador_ID_seq'::regclass),
 	username char(50) not null,
 	pontacao int not null,
+	CONSTRAINT "pkPontuacaoUtilizador" PRIMARY KEY (id, username),
 	CONSTRAINT "fkUsername_PontuacaoUtilizador" 
 	FOREIGN KEY (username) REFERENCES Utilizador (username) 
-	MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, 
+	MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 )
