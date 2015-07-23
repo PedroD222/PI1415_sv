dd33deeab5a929bef3361abb886bf6deb20328b9
JohnGod <joaodavid72@hotmail.com>
2015-07-23 10:01:35 +0100

    add sql code insertCategorias

---
+++ PF/bd/insertCategorias.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO Categoria (designacao) VALUES ( 'Tecnologia');
+INSERT INTO Categoria (designacao) VALUES ( 'Lazer');
+INSERT INTO Categoria (designacao) VALUES ( 'Desporto');
+INSERT INTO Categoria (designacao) VALUES ( 'Carros, motos e barcos');
+INSERT INTO Categoria (designacao) VALUES ( 'Moda');
+INSERT INTO Categoria (designacao) VALUES ( 'Imóveis');
+INSERT INTO Categoria (designacao) VALUES ( 'Móveis, Casa e Jardim');
+INSERT INTO Categoria (designacao) VALUES ( 'Outras Vendas');
